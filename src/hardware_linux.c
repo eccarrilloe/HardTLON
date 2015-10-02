163ad0a25dc5d67d877ec5b4f444269c6c79a751
Rodrigo Espinel <riespinelv@unal.edu.co>
2015-10-01 23:58:36 -0500

    hardware_linux.'x' modified new functionalities

--- src/hardware_linux.c
+++ src/hardware_linux.c
@@ -1,22 +1,75 @@
 #include "hardware_linux.h"
 struct sysinfo si;
+struct utsname unameData;
+struct statvfs stat;
+double mb = 1048576;
+
+getMemFree()
+{
+	FILE* fp = fopen( "/proc/meminfo", "r" );
+	if ( fp != NULL )
+	{
+		size_t bufsize = 1024 * sizeof(char);
+		char* buf      = (char*)malloc( bufsize );
+		long value     = -1L;
+		while ( getline( &buf, &bufsize, fp ) >= 0 )
+		{
+			if ( strncmp( buf, "MemFree", 7 ) != 0 )
+				continue;
+			sscanf( buf, "%ld",&value );
+			printf("%s", buf);
+			break;
+		}
+		fclose( fp );
+		free( (void*)buf );
+		if ( value != -1L )
+			return ((size_t)value * 1024L );
+	}
+}
 
 void getProcessorInformation()
 {
+	// The Linux kernel has a sysinfo() system call since 0.98.pl6.
 	sysinfo (&si);
+	uname(&unameData);
 	/*
 	printf ("system uptime : %ld days, %ld:%02ld:%02ld\n", 
 		si.uptime / day, (si.uptime % day) / hour, 
 		(si.uptime % hour) / minute, si.uptime % minute);
 	*/
+	printf("/*******************************************************/\n");
+	printf("Operating System:  %s\n", unameData.sysname);
+	printf("Release:  %s\n", unameData.release);
+	printf("Version:  %s\n", unameData.version);
+	printf("Architecture:  %s\n", unameData.machine);
 	printf("Number of precessors availables: %d\n", get_nprocs());
-}
+	printf("/*******************************************************/\n");
+}	
+ 
 
 void getMemoryInformation()
 {
-	double mb = 1048576;
-	printf("total Free RAM %.3f MB\n", si.totalram / mb);
+	printf("/*******************************************************/\n");
+
+	printf("Total usable main memory size: \t %.3f MB\n", si.totalram / mb);
+	getMemFree();
+	//printf("Available memory size %.5f MB\n", si.freeram / mb);
+	//printf("Amount of shared memory %.5f MB\n", si.sharedram / mb);
+	//printf("Memory used by buffers %.5f MB\n", si.bufferram / mb);
+	printf("Total swap space size: \t %.5f MB\n", si.totalswap / mb);
+	printf("/*******************************************************/\n");
+}
+
+void getHardDiskInformation()
+{
+	// Chequea solo una de las particiones la raíz "/"
+	// TODO Comprobar si hay más particiones del disco
+	statvfs("/",&stat);
+	printf("/*******************************************************/\n");
+	//printf("Total space %.3f MB \n", stat.f_frsize /mb);
+	printf("Total space %.3f MB\n", stat.f_bsize * stat.f_blocks /mb );
+	printf("Total free space: %.3f MB\n", stat.f_bsize * stat.f_bfree /mb );
+	printf("Free disk space for unprivileged users: %.3f MB\n", (stat.f_bsize * stat.f_bavail )/mb);
 }
 
 void getDisplayInformation(){}
-void getHardDiskInformation(){}
--- src/hardware_linux.h
+++ src/hardware_linux.h
@@ -1,7 +1,13 @@
 #ifndef HARDWARE_H_
 	#define HARDWARE_H_
 	#include <stdio.h>
+	#include <stdlib.h>
+	#include <unistd.h>
 	#include <sys/sysinfo.h>
+	#include <sys/statvfs.h>
+	#include <sys/utsname.h>
+	#include <gdk/gdk.h>
+	//#include <X11/Xlib.h>
 
 
 	void getProcessorInformation();
